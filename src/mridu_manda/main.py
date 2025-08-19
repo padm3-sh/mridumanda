bc71e8ca344fe6fbb5ebb9255d58f07944bc5023
Shvetank Dattatreya <hello.shvetank@gmail.com>
2025-08-19 21:55:50 +0530

    Added option to add and save API key

--- src/mridu_manda/main.py
+++ src/mridu_manda/main.py
@@ -1,17 +1,23 @@
 import requests
 import json
 import os
+from mridu_manda import setup_mridumanda
 
 
 def main():
+    setup_mridumanda.setup()
+    
     print("Welcome to MriduManda")
     city = input("Enter city name: ")
-    path_to_config = os.path.join(".sanctum", "config.json")
+    path_to_api = os.path.join(os.path.expanduser("~"), ".mridumanda", "api.txt")
+    api = None
     
-    with open (path_to_config, "r") as file:
-        config = json.load(file)
+    with open (path_to_api, "r") as file:
+        line = file.readline()
         
-    api = config['api_key']
+        if ":" in line:
+            key, value = line.strip().split(":", 1)
+            api = value
     
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}&units=metric"
     response = requests.get(url)
